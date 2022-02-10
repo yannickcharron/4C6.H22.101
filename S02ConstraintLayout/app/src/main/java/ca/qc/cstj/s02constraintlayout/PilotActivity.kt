ba9757ff4a1d637115894936989f2854d2e89c51
Yannick Charron <ycharron@cstj.qc.ca>
2022-02-10 11:30:15 -0500

    Fin du cours 10 février 2022

--- S02ConstraintLayout/app/src/main/java/ca/qc/cstj/s02constraintlayout/PilotActivity.kt
+++ S02ConstraintLayout/app/src/main/java/ca/qc/cstj/s02constraintlayout/PilotActivity.kt
@@ -2,10 +2,44 @@ package ca.qc.cstj.s02constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.qc.cstj.s02constraintlayout.databinding.ActivityPilotBinding
+import ca.qc.cstj.s02constraintlayout.domain.models.Pilot
 
 class PilotActivity : AppCompatActivity() {
+
+    //Permettre d'accéder aux composants graphiques (boutons, textview) de l'interface
+    private lateinit var binding: ActivityPilotBinding
+
+    private val _pilot = Pilot("Bee Zoom",10)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pilot)
+        // Charger le visual dans la objet binding
+        binding = ActivityPilotBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        refreshUI()
+
+        binding.btnStart.setOnClickListener {
+            if(_pilot.canFly()) {
+                _pilot.fly(binding.sldRevolution.value.toInt(), binding.swtTraining.isChecked)
+                refreshUI()
+            } else {
+                //TODO: SNACKBAR
+            }
+        }
+
+    }
+
+    private fun refreshUI() {
+
+        with(binding) {
+            txvPilotName.text = _pilot.name
+            txvLevel.text = _pilot.level.toString()
+            txvCube.text = _pilot.cube.toString()
+            txvEnergy.text = _pilot.energy.toString()
+            txvShield.text = _pilot.shield.toString()
+            txvLife.text = _pilot.life.toString()
+        }
     }
 }
---
+++ S02ConstraintLayout/app/src/main/java/ca/qc/cstj/s02constraintlayout/domain/models/Pilot.kt
@@ -0,0 +1,31 @@
+package ca.qc.cstj.s02constraintlayout.domain.models
+
+import kotlin.random.Random
+
+data class Pilot(var name: String, var life: Int, var cube: Int = 0) {
+
+    private var _experience: Int = 0
+    var shield: Int = 10
+    var energy: Int = 10
+
+    val level: Int get() = _experience / 10
+
+    fun fly(revolutions: Int, isTraining: Boolean) {
+        if (!isTraining) {
+            _experience += revolutions * Random.nextInt(1, level + 2)
+            life -= Random.nextInt(0, 2)
+            shield -= Random.nextInt(0, 2)
+            energy -= 1
+        }
+    }
+
+    fun canFly(): Boolean {
+        return life > 0 && energy > 0
+    }
+
+    fun recharge() {
+        //TODO: Add microtransaction
+        energy = 10
+    }
+
+}
