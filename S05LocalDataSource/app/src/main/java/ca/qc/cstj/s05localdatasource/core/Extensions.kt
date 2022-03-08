c253c7cea2be8ca25fec62a707953036d8e43e36
Yannick Charron <ycharron@cstj.qc.ca>
2022-03-08 16:42:11 -0500

    Fin du cours - 8 mars 2022

--- S05LocalDataSource/app/src/main/java/ca/qc/cstj/s05localdatasource/core/Extensions.kt
+++ S05LocalDataSource/app/src/main/java/ca/qc/cstj/s05localdatasource/core/Extensions.kt
@@ -1,1 +1,8 @@
 package ca.qc.cstj.s05localdatasource.core
+
+import androidx.recyclerview.widget.RecyclerView
+
+
+fun <VH: RecyclerView.ViewHolder> RecyclerView.Adapter<VH>.notifyAllItemChanged() {
+    notifyItemRangeChanged(0,itemCount)
+}
--- S05LocalDataSource/app/src/main/java/ca/qc/cstj/s05localdatasource/data/repositories/ContactRepository.kt
+++ S05LocalDataSource/app/src/main/java/ca/qc/cstj/s05localdatasource/data/repositories/ContactRepository.kt
@@ -1,17 +1,35 @@
 package ca.qc.cstj.s05localdatasource.data.repositories
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 import ca.qc.cstj.s05localdatasource.domain.models.Contact
+import kotlinx.coroutines.flow.Flow
 
-class ContactRepository {
+@Dao
+interface ContactRepository {
 
-//    fun retrieveAll(numContacts: Int): List<Contact> {
-//
-//        val contacts = mutableListOf<Contact>()
-//        for (i in 1..numContacts) {
-//            contacts.add(Contact("FirstName $i", "LastName $i", i % 2 == 0))
-//        }
-//
-//        return contacts
-//    }
+    @Query("SELECT * FROM contacts")
+    fun retrieveAll(): Flow<List<Contact>>
+
+    @Insert
+    fun create(contacts: List<Contact>)
+
+    @Insert
+    suspend fun create(contact: Contact)
 
 }
+
+//https://medium.com/androiddevelopers/room-coroutines-422b786dc4c5
+//fun retrieveAll(numContacts: Int): List<Contact> {
+//
+//    val contacts = mutableListOf<Contact>()
+//
+//    contacts.add(Contact("Yannick", "Charron", true))
+//
+//    for (i in 1..numContacts) {
+//        contacts.add(Contact("FirstName $i", "LastName $i", i % 2 == 0))
+//    }
+//
+//    return contacts
+//}
